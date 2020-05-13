669df561d208ff86f83f2133bbca6017c2f9cb44
Inkit <inkitter@gmail.com>
2020-05-13 22:42:18 +0800

    update hoi1.9

--- release/HOI4.bat
+++ release/HOI4.bat
@@ -1,5 +1,5 @@
 rd eng /s /q
 rd chn /s /q
-mklink /j eng "old\zhoi-1.4"
+mklink /j eng "old\zhoi-1.9"
 md chn
-mklink /j chn\english "E:\HOI4_mod\_Inkitmod\chnLocal\localisation"
+mklink /j chn\english "D:\Git\HOI4_mod\_Inkitmod\chnLocal\localisation"
